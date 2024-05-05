d8c858f0b68de3ce5d877fbf501fa2692d45eb48
kerenelbaz <132551849+kerenelbaz@users.noreply.github.com>
2024-05-05 23:04:05 +0300

    changed diveMap
    changed the select to tap and update the dive background color

--- src/layouts/dashboard/header.jsx
+++ src/layouts/dashboard/header.jsx
@@ -1,23 +1,25 @@
+// import { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
+
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import { useTheme } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+
+import { RouterLink } from 'src/routes/components';
 
 import { useResponsive } from 'src/hooks/use-responsive';
-import Typography from '@mui/material/Typography';
+
 import { bgBlur } from 'src/theme/css';
-import { RouterLink } from 'src/routes/components';
+
 import Iconify from 'src/components/iconify';
 
 // import Searchbar from './common/searchbar';
 import { NAV, HEADER } from './config-layout';
 import AccountPopover from './common/account-popover';
-import LanguagePopover from './common/language-popover';
-import NotificationsPopover from './common/notifications-popover';
 
 // ----------------------------------------------------------------------
 
