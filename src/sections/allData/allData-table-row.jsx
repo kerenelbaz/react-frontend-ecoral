ff1f02a61745a3a766508fe2a8d0033349aa016b
kerenelbaz <Keren.Elbaz@ruppin365.net>
2024-07-24 22:46:49 +0300

    Update allData-table-row.jsx
    added icon button

--- src/sections/allData/allData-table-row.jsx
+++ src/sections/allData/allData-table-row.jsx
@@ -3,33 +3,26 @@ import PropTypes from 'prop-types';
 
 import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
-import Popover from '@mui/material/Popover';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
-import Typography from '@mui/material/Typography';
+import EditIcon from '@mui/icons-material/Edit';
 import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 import ImageDialog from './imageDialog';
-
 
 // ----------------------------------------------------------------------
 
 export default function AllDataTableRow({
   selected, name, avatarUrl, company, role, isVerified, status, handleClick,
-  diveCode, diveDate,time, diveSite, objectGroup, specie, idCode, location, ar, humanWildInter, 
-  reportType, typeOfDive, groupCode, media, fileType, reportRecivingDate, InvestDoc, diverAge, diverSex, rank,loggedBy, loggingDate,
-  idSharks, distance, temp,maxDepth, userDesc, researcherDesc ,fileLink,onDeleteClicked,
+  diveCode, diveDate, time, diveSite, objectGroup, specie, idCode, location, ar, humanWildInter, 
+  reportType, typeOfDive, groupCode, media, fileType, reportRecivingDate, InvestDoc, diverAge, diverSex, rank, loggedBy, loggingDate,
+  idSharks, distance, temp, maxDepth, userDesc, researcherDesc, fileLink, onDeleteClicked, onEditClicked
 }) {
-  const [open, setOpen] = useState(null);
   const [dialogOpen, setDialogOpen] = useState(false);
 
-
-  const handleCloseMenu = () => {
-    setOpen(null);
-  };
-
-  const handleDelete = ()=>{
+  const handleDelete = () => {
     onDeleteClicked();
   }
 
@@ -41,20 +34,21 @@ export default function AllDataTableRow({
     setDialogOpen(false);
   };
 
-
   return (
     <>
-      <TableRow >
-        <TableCell padding="checkbox" >
-        <IconButton aria-label="delete" size="small" color='error' onClick={handleDelete}>
-          <DeleteIcon />
-          </IconButton>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Stack direction="row" spacing={1}>
+            <IconButton aria-label="edit" size="small" color='primary' onClick={onEditClicked}>
+              <EditIcon />
+            </IconButton>
+            <IconButton aria-label="delete" size="small" color='error' onClick={handleDelete}>
+              <DeleteIcon />
+            </IconButton>
+          </Stack>
         </TableCell>
-
         <TableCell component="th" scope="row" padding="3px">
-
           <Stack direction="row" alignItems="center" spacing={4}>
-            {/* <Avatar alt={diveCode} src={avatarUrl} /> */}
             <Typography variant="subtitle2" noWrap>
               {diveCode}
             </Typography>
@@ -84,7 +78,6 @@ export default function AllDataTableRow({
         <TableCell>{userDesc}</TableCell>
         <TableCell>{researcherDesc}</TableCell>
         <TableCell>{loggedBy}</TableCell>
-        {/* <TableCell>{fileLink}</TableCell> */}
         <TableCell>
           <Stack direction="row" alignItems="center" spacing={4}>
             <Avatar alt={diveCode} src={fileLink} onClick={handleAvatarClick} sx={{ cursor: 'pointer' }} />
@@ -92,27 +85,6 @@ export default function AllDataTableRow({
         </TableCell>
       </TableRow>
       <ImageDialog open={dialogOpen} onClose={handleCloseDialog} fileLink={fileLink} />
-
-      <Popover
-        open={!!open}
-        anchorEl={open}
-        onClose={handleCloseMenu}
-        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        PaperProps={{
-          sx: { width: 140 },
-        }}
-      >
-        {/* <MenuItem onClick={handleCloseMenu}>
-          <Iconify icon="eva:edit-fill" sx={{ mr: 2 }} />
-          Edit
-        </MenuItem>
-
-        <MenuItem onClick={handleCloseMenu} sx={{ color: 'error.main' }}>
-          <Iconify icon="eva:trash-2-outline" sx={{ mr: 2 }} />
-          Delete
-        </MenuItem> */}
-      </Popover>
     </>
   );
 }
@@ -126,34 +98,35 @@ AllDataTableRow.propTypes = {
   role: PropTypes.any,
   selected: PropTypes.any,
   status: PropTypes.string,
-  diveCode:PropTypes.string,
-  diveDate:PropTypes.string, 
-  diveSite:PropTypes.string, 
-  objectGroup:PropTypes.string, 
-  specie:PropTypes.string, 
-  idCode:PropTypes.string, 
-  location:PropTypes.string, 
-  ar:PropTypes.string, 
-  humanWildInter:PropTypes.string, 
-  reportType:PropTypes.string, 
-  typeOfDive:PropTypes.string, 
-  groupCode:PropTypes.string, 
-  media:PropTypes.string, 
-  fileType:PropTypes.string, 
-  InvestDoc:PropTypes.string, 
-  diverAge:PropTypes.string, 
-  diverSex:PropTypes.string, 
-  idSharks:PropTypes.string, 
-  distance:PropTypes.string, 
-  temp:PropTypes.string,
-  maxDepth:PropTypes.string, 
-  userDesc:PropTypes.string, 
-  researcherDesc:PropTypes.string, 
-  fileLink:PropTypes.string,
-  reportRecivingDate:PropTypes.string,
-  rank:PropTypes.string,
-  loggedBy:PropTypes.string,
-  loggingDate:PropTypes.string,
-  time:PropTypes.string,
-  onDeleteClicked:PropTypes.func,
+  diveCode: PropTypes.string,
+  diveDate: PropTypes.string, 
+  diveSite: PropTypes.string, 
+  objectGroup: PropTypes.string, 
+  specie: PropTypes.string, 
+  idCode: PropTypes.string, 
+  location: PropTypes.string, 
+  ar: PropTypes.string, 
+  humanWildInter: PropTypes.string, 
+  reportType: PropTypes.string, 
+  typeOfDive: PropTypes.string, 
+  groupCode: PropTypes.string, 
+  media: PropTypes.string, 
+  fileType: PropTypes.string, 
+  InvestDoc: PropTypes.string, 
+  diverAge: PropTypes.string, 
+  diverSex: PropTypes.string, 
+  idSharks: PropTypes.string, 
+  distance: PropTypes.string, 
+  temp: PropTypes.string,
+  maxDepth: PropTypes.string, 
+  userDesc: PropTypes.string, 
+  researcherDesc: PropTypes.string, 
+  fileLink: PropTypes.string,
+  reportRecivingDate: PropTypes.string,
+  rank: PropTypes.string,
+  loggedBy: PropTypes.string,
+  loggingDate: PropTypes.string,
+  time: PropTypes.string,
+  onDeleteClicked: PropTypes.func,
+  onEditClicked: PropTypes.func,
 };
