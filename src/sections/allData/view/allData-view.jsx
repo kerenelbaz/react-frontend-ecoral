3c9e31c2e954dbfe7b1b217d30e459ee97f8e468
kerenelbaz <Keren.Elbaz@ruppin365.net>
2024-07-25 11:51:22 +0300

    Update allData-view.jsx

--- src/sections/allData/view/allData-view.jsx
+++ src/sections/allData/view/allData-view.jsx
@@ -1,50 +1,45 @@
 import { useState, useEffect } from 'react';
-// import { users } from 'src/_mock/user';
 import { useNavigate } from 'react-router-dom';
 
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Table from '@mui/material/Table';
 import Button from '@mui/material/Button';
+// import Dialog from '@mui/material/Dialog';
 import Container from '@mui/material/Container';
 import TableBody from '@mui/material/TableBody';
 import Typography from '@mui/material/Typography';
+// import DialogTitle from '@mui/material/DialogTitle';
+// import DialogActions from '@mui/material/DialogActions';
+// import DialogContent from '@mui/material/DialogContent';
 import TableContainer from '@mui/material/TableContainer';
 import TablePagination from '@mui/material/TablePagination';
+// import DialogContentText from '@mui/material/DialogContentText';
 
 import { useView } from 'src/viewContexts';
 
 import Iconify from 'src/components/iconify';
-// import Iconify from 'src/components/iconify';
 import Scrollbar from 'src/components/scrollbar';
 
-// eslint-disable-next-line no-unused-vars, unused-imports/no-unused-imports
-import tableStyle from './tableStyle.css';
+import EditData from 'src/sections/pendingAdmin/view/handle-edit-data';
+
 import AllDataTableRow from '../allData-table-row';
 import TableNoData from '../allData-table-no-data';
 import AllDataTableHead from '../allData-table-head';
 import TableEmptyRows from '../allData-table-empty-rows';
-// import AllDataTableTollbar from '../allData-table-toolbar';
-import { emptyRows, applyFilter, getComparator } from '../utils';
-// ----------------------------------------------------------------------
-
+import { emptyRows, applyFilter, getComparator } from '../utils'; // Adjust the import path as needed
 
 export default function AllDataView() {
   const [page, setPage] = useState(0);
-
   const [order, setOrder] = useState('asc');
-
   const [selected, setSelected] = useState([]);
-
   const [orderBy, setOrderBy] = useState('name');
-
   const [filterName] = useState('');
-
   const [rowsPerPage, setRowsPerPage] = useState(5);
-
   const [allDataDives, setAllDataDives] = useState([]);
   const [loading, setLoading] = useState(true); // Define loading state
-  const [setEditingRow] = useState(null);
+  const [editingRow, setEditingRow] = useState(null); // State for editing row
+  const [editDialogOpen, setEditDialogOpen] = useState(false); // State for edit dialog
   const { switchToBlog } = useView();
   const navigate = useNavigate();
 
@@ -56,30 +51,23 @@ export default function AllDataView() {
           throw new Error('Failed to fetch data');
         }
         const responseData = await response.json();
-        console.log(responseData.data);
         const { dives } = responseData.data;
-        
         dives.sort((a, b) => new Date(b.logginDate) - new Date(a.logginDate));
-
         setAllDataDives(dives);
-        console.log("inside useEffect the allDataDives is:", allDataDives)
         setLoading(false); // Data fetched successfully
       } catch (error) {
         console.error('Error fetching data:', error);
         setLoading(false); // Error occurred while fetching data
       }
     };
-
     fetchData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); 
+  }, []);
 
   if (loading) {
     return <div>Loading...</div>; // Render loading indicator while data is being fetched
   }
 
   if (!allDataDives || allDataDives.length === 0) {
-    console.log("allDataDives:", allDataDives);
     return <div>No data available</div>; // Render message if data is empty
   }
 
@@ -88,28 +76,17 @@ export default function AllDataView() {
     navigate('/dynamic-view');
   };
 
-  // const handleSort = (event, id) => {
-  //   const isAsc = orderBy === id && order === 'asc';
-  //   if (id !== '') {
-  //     setOrder(isAsc ? 'desc' : 'asc');
-  //     setOrderBy(id);
-  //   }
-  // };
   const handleSort = (event, id) => {
     const isAsc = orderBy === id && order === 'asc';
     if (id !== '') {
       setOrder(isAsc ? 'desc' : 'asc');
       setOrderBy(id);
-  
-      // Sort the allDataDives array based on the selected column and order
       const sortedData = allDataDives.slice().sort((a, b) => {
         if (isAsc) {
           return a[id] > b[id] ? 1 : -1;
         } 
-          return a[id] < b[id] ? 1 : -1;
-        
+        return a[id] < b[id] ? 1 : -1;
       });
-  
       setAllDataDives(sortedData);
     }
   };
@@ -150,81 +127,70 @@ export default function AllDataView() {
     setRowsPerPage(parseInt(event.target.value, 10));
   };
 
-
   const dataFiltered = applyFilter({
     inputData: allDataDives,
     comparator: getComparator(order, orderBy),
     filterName,
   });
 
-  // eslint-disable-next-line no-unused-vars
   const formatDateTime = (dateTimeString) => {
-   // Regular expressions to match the expected date formats
     const dateFormatRegex1 = /^\w{3} \w{3} \d{2} \d{4} \d{2}:\d{2}:\d{2} GMT[+-]\d{4} \([\w\s]+\)$/;
     const dateFormatRegex2 = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/;
-
     if (!dateFormatRegex1.test(dateTimeString) && !dateFormatRegex2.test(dateTimeString)) {
       return dateTimeString; 
     }
-
-
     const dateTime = new Date(dateTimeString);
-
     const dateFormatter = new Intl.DateTimeFormat('en-GB', {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
     });
     const formattedDate = dateFormatter.format(dateTime);
-
     const timeFormatter = new Intl.DateTimeFormat('en-GB', {
       hour: '2-digit',
       minute: '2-digit',
     });
     const formattedTime = timeFormatter.format(dateTime);
-
     return `${formattedDate}, ${formattedTime}`;
   };
 
   const handleDeleteClick = async (rowClicked) => {
-    console.log(rowClicked)
-    console.log(rowClicked._id);
     try {
-    
       const response = await fetch(`http://localhost:8000/api/Dives/${rowClicked._id}`, {
         method: 'DELETE',
       });
       if (!response.ok) {
         throw new Error('Failed to delete row');
       }
-
       setAllDataDives(prevData => prevData.filter(row => row._id !== rowClicked._id));
-
     } catch (error) {
       console.error('Error deleting row:', error);
     }
-
   };
 
   const handleEditClick = (row) => {
     setEditingRow(row);
+    setEditDialogOpen(true);
   };
-  
+
+  const handleEditClose = () => {
+    setEditDialogOpen(false);
+    setEditingRow(null);
+  };
+
   const notFound = !dataFiltered.length && !!filterName;
 
   return (
     <Container className="custom-container">
       <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
         <Typography variant="h4">All Dives Data</Typography>
-          <Button variant="contained" color="inherit" startIcon={<Iconify icon="mdi:table" />} onClick={handleSwitchToBlog}>
-            As Cards
-          </Button>
+        <Button variant="contained" color="inherit" startIcon={<Iconify icon="mdi:table" />} onClick={handleSwitchToBlog}>
+          As Cards
+        </Button>
       </Stack>
-
       <Card>
-
         <Scrollbar>
-          <TableContainer sx={{ overflow: 'unset', maxWiwdth: '2500px' }}>
+          <TableContainer sx={{ overflow: 'unset', maxWidth: '2500px' }}>
             <Table sx={{ minWidth: '2000px', width: '100%' }}>
               <AllDataTableHead
                 order={order}
@@ -241,9 +207,9 @@ export default function AllDataView() {
                   { id: 'time', label: 'Time' },
                   { id: 'diveSite', label: 'Dive Site' },
                   { id: 'objectGroup', label: 'Object Group', align: 'center' },
-                  { id: 'specie', label: 'specie' },
+                  { id: 'specie', label: 'Specie' },
                   { id: 'idCode', label: 'ID Code' },
-                  { id: 'Location', label: 'location' },
+                  { id: 'Location', label: 'Location' },
                   { id: 'ar', label: 'Is AR?' },
                   { id: 'humanWildInter', label: 'Human-Wild Interaction' },
                   { id: 'reportType', label: 'Report Type' },
@@ -258,12 +224,9 @@ export default function AllDataView() {
                   { id: 'temp', label: 'Temperature' },
                   { id: 'userDesc', label: 'User Description' },
                   { id: 'researcherDesc', label: 'Researcher Comments' },
-                  { id: 'logged By', label: 'Logged By' },
+                  { id: 'loggedBy', label: 'Logged By' },
                   { id: 'fileLink', label: 'Link' },
-                  // { id: 'reportRecivingDate', label: 'Report reciving Date' },
                   { id: '' },
-
-
                 ]}
               />
               <TableBody>
@@ -299,22 +262,19 @@ export default function AllDataView() {
                       loggedBy={row.loggedBy}
                       fileLink={row.linkURL}
                       handleClick={(event) => handleClick(event, row.name)}
-                      onDeleteClicked={()=> handleDeleteClick(row)}
+                      onDeleteClicked={() => handleDeleteClick(row)}
                       onEditClicked={() => handleEditClick(row)}
                     />
                   ))}
-
                 <TableEmptyRows
                   height={77}
                   emptyRows={emptyRows(page, rowsPerPage, allDataDives.length)}
                 />
-
                 {notFound && <TableNoData query={filterName} />}
               </TableBody>
             </Table>
           </TableContainer>
         </Scrollbar>
-
         <TablePagination
           page={page}
           component="div"
@@ -326,6 +286,13 @@ export default function AllDataView() {
           onRowsPerPageChange={handleChangeRowsPerPage}
         />
       </Card>
+
+      <EditData
+        open={editDialogOpen}
+        handleClose={handleEditClose}
+        pendingData={editingRow}
+        onDeleteClick={() => {}}
+      />
     </Container>
   );
 }
